fd60599983ec2bb4e891f5f105aa563f1174cf27
LonelyGlare <118815680+LonelyGlare@users.noreply.github.com>
2023-04-07 01:40:32 +0200

    sql correction

--- docker/init_db.sh
+++ docker/init_db.sh
@@ -1,10 +1,11 @@
 mysql -u root -p$MYSQL_ROOT_PASSWORD <<_EOF
-CREATE DATABASE partidos_futbol;
+CREATE DATABASE IF NOT EXISTS partidos_futbol;
 USE partidos_futbol;
-CREATE TABLE partidos (
+CREATE TABLE IF NOT EXISTS partidos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     equipo_local VARCHAR(255),
     equipo_visitante VARCHAR(255),
     goles_local INT,
     goles_visitante INT
 );
+_EOF
