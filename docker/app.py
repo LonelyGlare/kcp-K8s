ea240f6c5d78ca2ead72a8b45da200ceaa6452f0
LonelyGlare <118815680+LonelyGlare@users.noreply.github.com>
2023-04-07 12:36:17 +0200

    sql errors

--- docker/app.py
+++ docker/app.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 app.config['MYSQL_DB'] = os.environ['MYSQL_DATABASE'] or 'db-partidos'
-app.config['MYSQL_USER'] = os.environ['MYSQL_USER'] or 'mysql'
+app.config['MYSQL_USER'] = os.environ['MYSQL_USER'] or 'user'
 app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD'] or 'user'
 app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST'] or 'db'
 mysql = MySQL(app)
