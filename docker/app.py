1b05c0b44ad39e75765ecf943d7ddaecb8f5df2f
LonelyGlare <118815680+LonelyGlare@users.noreply.github.com>
2023-04-07 02:00:00 +0200

    sql corrections

--- docker/app.py
+++ docker/app.py
@@ -17,16 +17,16 @@ def crear_partidos():
     cursor = mysql.connection.cursor()
     cursor.execute(''' DROP TABLE IF EXISTS partidos; ''')
     cursor.execute(''' CREATE TABLE partidos (id INT PRIMARY KEY AUTO_INCREMENT, equipo_local VARCHAR(255), equipo_visitante VARCHAR(255), resultado VARCHAR(255), fecha DATETIME); ''')
+    fecha = datetime.datetime.now()
     for i in range(10):
         local = random.choice(equipos)
         visitante = random.choice([x for x in equipos if x != local])
         resultado = f"{random.randint(0, 5)} - {random.randint(0, 5)}"
         fecha_str = fecha.strftime('%Y-%m-%d %H:%M:%S')
-    cursor.execute(f''' INSERT INTO partidos (equipo_local, equipo_visitante, resultado, fecha) VALUES ('{local}', '{visitante}', '{resultado}', '{fecha_str}'); ''')
+        cursor.execute(f''' INSERT INTO partidos (equipo_local, equipo_visitante, resultado, fecha) VALUES ('{local}', '{visitante}', '{resultado}', '{fecha_str}'); ''')
     cursor.execute(''' COMMIT; ''')
     cursor.close()
     return 'Partidos creados'
-
 
 @app.route('/')
 def mostrar_partidos():
